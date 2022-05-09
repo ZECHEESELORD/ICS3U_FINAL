4ddfdb29e4e10506bee7ffb872ba5a677627dcba
urboiJeff2006 <52462206+urboiJeff2006@users.noreply.github.com>
2022-05-09 14:21:00 -0400

    qi3opth3iop2t

--- src/main/java/org/crescentschool/ics3ufinal/HelloApplication.java
+++ src/main/java/org/crescentschool/ics3ufinal/HelloApplication.java
@@ -15,6 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        //jeffryu bad at git
     }
 
     public static void main(String[] args) {
